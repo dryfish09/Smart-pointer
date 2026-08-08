38708ef6eae68a6c7d00873d11879b7b1151b396
DryFish <realmg51@gmail.com>
2026-08-08 17:40:39 +0700

    Create move_unique_ptr_to_function.cpp

---
+++ src/unique_ptr/move_unique_ptr_to_function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+class ClassRoom {
+public:
+    ClassRoom() {
+        std::cout<<"Teacher is entering, stand up!"<<std::endl;
+    }
+    ~ClassRoom() {
+        std::cout<<"Teacher is leaving!"<<std::endl;
+    }
+    void teach() {
+        std::cout<<"Teacher is teaching...."<<std::endl;
+    }
+};
+void takeOwnerShip(std::unique_ptr<ClassRoom> teacher) {
+    std::cout<<"Teacher A is in class right now!"<<std::endl;
+}
+int main() {
+    std::cout<<"Starting class..."<<std::endl;
+    {
+        auto p = std::unique_ptr<ClassRoom>();
+        takeOwnerShip(std::move(p));
+    }
+    return 0;
+}
