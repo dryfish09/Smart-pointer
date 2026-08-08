1b4feecb0c7255fb283f2e15f5a70256f8dcc178
DryFish <realmg51@gmail.com>
2026-08-09 06:36:18 +0700

    etc: add comments

--- src/unique_ptr/unique_ptr_basic.cpp
+++ src/unique_ptr/unique_ptr_basic.cpp
@@ -4,17 +4,20 @@
 #include <memory>
 class ClassRoomInit {
 public:
+    // constructor method
     ClassRoomInit() {
          std::cout<<"Teacher is entering, stand up!"<<std::endl;
      }
+     // destructor method (same as class name and constructor but add '~' before nsmame like ~SomeThing)
     ~ClassRoomInit() {
         std::cout<<"Teacher is out, let's play!"<<std::endl;
     }
 };
 int main() {
 std::cout<<"Class begin!"<<std::endl;
+    // starting scope
     {
        std::unique_ptr<ClassRoomInit> p = std::make_unique<ClassRoomInit>();
-    }
+    } // pointer will delete if it's out-of-scope, so we no need to worry about dangling ptr, UAF,.. That's wy we call it smart pointer.
     return 0;
 }
