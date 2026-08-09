c3325213cee7a77f0027ebba49cf2a3111b1f40a
DryFish <realmg51@gmail.com>
2026-08-09 07:13:49 +0700

    etc: add comment

--- src/unique_ptr/unique_ptr_move_pointer.cpp
+++ src/unique_ptr/unique_ptr_move_pointer.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <memory>
 class ClassRoomInit {
+// this part is same as file 'unique_ptr_basic.cpp'
 public:
     ClassRoomInit() {
          std::cout<<"Teacher is entering, stand up!"<<std::endl;
@@ -15,7 +16,9 @@ int main() {
 std::cout<<"Class begin!"<<std::endl;
     {
        std::unique_ptr<ClassRoomInit> p = std::make_unique<ClassRoomInit>();
+       // create a new pointer also point to constructor like first pointer:
        std::unique_ptr<ClassRoomInit> pointerlol;
+        // move permission to new pointer. Do not copy like pointerlol = p
        pointerlol = std::move(p); // moved permission!
     }
     return 0;
