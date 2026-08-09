09f7c6f78c3b29c78e205dd84c51aa39f9d1d0ce
DryFish <realmg51@gmail.com>
2026-08-09 07:18:12 +0700

    Update move_unique_ptr_to_function.cpp

--- src/unique_ptr/move_unique_ptr_to_function.cpp
+++ src/unique_ptr/move_unique_ptr_to_function.cpp
@@ -10,18 +10,23 @@ public:
     ~ClassRoom() {
         std::cout<<"Teacher is leaving!"<<std::endl;
     }
+    // object's method.
     void teach() {
         std::cout<<"Teacher is teaching...."<<std::endl;
     }
 };
+// idk what's it means rn
+// todo: delete this line
 void takeOwnerShip(std::unique_ptr<ClassRoom> teacher) {
     std::cout<<"Teacher is in class right now!"<<std::endl;
+    // call function:
     teacher->teach();
 }
 int main() {
     std::cout<<"Starting class..."<<std::endl;
     {
         std::unique_ptr<ClassRoom> p = std::make_unique<ClassRoom>();
+        // idk how to explain it. todo: delete this line
         takeOwnerShip(std::move(p));
         std::cout<<"teacher changed!"<<std::endl;
     }
