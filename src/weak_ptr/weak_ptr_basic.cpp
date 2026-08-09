8fbb54bfd2e057bb07f7ed056133b4aaf4325051
DryFish <realmg51@gmail.com>
2026-08-09 18:57:58 +0700

    Complete code

--- src/weak_ptr/weak_ptr_basic.cpp
+++ src/weak_ptr/weak_ptr_basic.cpp
@@ -13,17 +13,22 @@ public:
     void teach() {
         std::cout<<"Teaching..."<<std::endl;
     }
-}
+};
 int main() {
     std::cout<<"Class started!"<<std::endl;
     {
         // create shared pointer (owner):
         std::shared_ptr<ClassRoom> owner = std::make_shared<ClassRoom>();
         // create a weak pointer (watcher):
-        std::weak_ptr<ClassRoom> = owner; // it will not increase use_count number.
+        std::weak_ptr<ClassRoom> watcher = owner; // it will not increase use_count number.
         // check how many pointers are using owner shared pointer:
         std::cout<<"Used time: " << owner.use_count() <<std::endl; // will be 1 (itself)
         // is object alive? Let's check!
         if (watcher.expired) {
             std::cout<<"object died!"<<std::endl;
+        } else {
+            std::cout<<"Object still alive"<<std::endl;
         }
+    }
+    return 0;
+}
