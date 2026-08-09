8b66150bb3ede44251210319dbad1f23972ebbc5
DryFish <realmg51@gmail.com>
2026-08-09 12:36:24 +0700

    Commit to prevent lost progress

---
+++ src/shared_ptr/shared_ptr_basic.cpp
@@ -0,0 +1,26 @@
+// created by dryfish09. The guy who know a bit C++.
+// filename: shared_ptr_basic.cpp
+#include <iostream>
+#include <memory>
+class ClassRoom {
+public:
+    ClassRoom() {
+        std::cout <<"teacher is entering, stand up!"<< std::endl;
+    }
+    ~ClassRoom() {
+       std::cout <<"teacher is leaving, time to rest!"<<std::endl;
+    }
+    void teaching() {
+        std::cout <<"teacher is teaching!"<< std::endl;
+    }
+};
+int main() {
+    // print satus:
+    std::cout<<"class started!"<<std::endl;
+    std::shared_ptr<ClassRoom> teacher1 = std::make_shared<ClassRoom>();
+    // count how many teacher are using this teacher's permission
+    std::cout<<"Used: " << teacher1.use_count() << " times." <<std::endl;
+    {
+        // shared_ptr allows copy!
+        std::shared_ptr<ClassRoom> teacher2 = teacher1;
+        cout<<"Used: " << teacher1.use_count() << " times." << std::endl; // must be 2
