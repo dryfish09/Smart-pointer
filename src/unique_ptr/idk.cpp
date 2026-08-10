9769782e506a21decb348a3fbf4f58dec05ac4d0
DryFish <realmg51@gmail.com>
2026-08-10 07:40:01 +0700

    Create idk.cpp

---
+++ src/unique_ptr/idk.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+
+class Student {
+public:
+    std::string name;
+    Student(const std::string& n) : name(n) {
+        std::cout << name << " enters class!" << std::endl;
+    }
+    ~Student() {
+        std::cout << name << " leaves class!" << std::endl;
+    }
+};
+int main() {
+    std::cout<<"Class begins!"<<std::endl;
+    {
+        std::unique_ptr<Student> Jack = std::make_unique<Student>("Jack");
+        std::cout<<"Current student: Jack"<<std::endl;
+        std::unique_ptr<Student> Mary = std::move(Jack);
+        std::cout<<"unique_ptr changed to mary"<<std::endl;
+    }
+}
